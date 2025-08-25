c54c32fc2f85f9c9f66739cf01312b480c894ac0
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-25 11:12:31 +0530

    same case sensitive

--- Frontend/src/components/Ui.jsx
+++ Frontend/src/components/Ui.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './navbar'
+import Navbar from './Navbar'
 import CodeBlock from './CodeBlock';
 import axios from 'axios'
 import { useState } from 'react';
