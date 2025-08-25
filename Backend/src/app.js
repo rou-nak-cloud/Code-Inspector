a298f265be3808c90a95c3b4d499081657ea1b8b
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-25 11:01:44 +0530

    production ready build

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,22 @@
+import path from 'path'
 import express from 'express'
 import aiRoutes from './routes/ai.route.js'
 import cors from 'cors'
 
 const app = express();
+const _dirname = path.resolve()
+
 app.use(express.json());
 app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.send('Running')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Running')
+// })
 app.use('/api/ai',aiRoutes)
+
+app.use(express.static(path.join(_dirname, "/Frontend/dist")))
+app.get('*', (_,res)=>{
+    res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"))
+})
 
 export default app;
